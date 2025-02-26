b441db35cab3b7483f5817ed01c5a073cfb1bdb4
AustereTriceratops <gatewaylevi@gmail.com>
2025-02-25 19:12:02 -0800

    now using react compiler

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react({
+    babel: {plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]]}
+  })],
   esbuild: {
     loader: 'jsx',
   },
